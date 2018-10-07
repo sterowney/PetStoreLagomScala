1694b1d52e5be0fe4420e89b03285dfe61aac473
Steven Rowney <steven@thebluecabrio.com>
2018-10-07 22:17:15 +0200

    Added Read Side processor for pets

---
+++ pet-impl/src/main/scala/com/sterowney/pet/impl/PetEventReadSideProcessor.scala
@@ -0,0 +1,66 @@
+package com.sterowney.pet.impl
+
+import akka.Done
+import com.datastax.driver.core.PreparedStatement
+import com.lightbend.lagom.scaladsl.persistence.cassandra.{CassandraReadSide, CassandraSession}
+import com.lightbend.lagom.scaladsl.persistence.{AggregateEventTag, ReadSideProcessor}
+
+import scala.concurrent.{ExecutionContext, Future, Promise}
+
+class PetEventReadSideProcessor(session: CassandraSession, readSide: CassandraReadSide)(implicit ec: ExecutionContext)
+  extends ReadSideProcessor[PetEvent] {
+
+  private val insertPetStatementPromise = Promise[PreparedStatement]
+  private def insertPetStatement: Future[PreparedStatement] = insertPetStatementPromise.future
+
+  override def buildHandler(): ReadSideProcessor.ReadSideHandler[PetEvent] = {
+    readSide.builder[PetEvent]("petEventOffset")
+      .setGlobalPrepare(createTables)
+      .setPrepare(_ => prepareStatements())
+      .setEventHandler[PetCreated](e => insertPet(e.event.pet))
+      .build
+  }
+
+  override def aggregateTags: Set[AggregateEventTag[PetEvent]] = PetEvent.Tag.allTags
+
+  private def createTables() = {
+    for {
+      _ <- session.executeCreateTable("""
+        CREATE TABLE IF NOT EXISTS pet (
+          petId timeuuid PRIMARY KEY,
+          name text,
+          categoryId double
+        )
+      """)
+    } yield Done
+  }
+
+  private def prepareStatements() = {
+    val insertPetFuture = session.prepare(
+      """
+          INSERT INTO pet(petId, name, categoryId) VALUES (?, ?, ?)
+        """)
+
+    insertPetStatementPromise.completeWith(insertPetFuture)
+
+    for {
+      _ <- insertPetFuture
+    } yield Done
+  }
+
+  private def doInsertPet(pet: Pet) = {
+    insertPetStatement.map { ps =>
+      val bindPet = ps.bind()
+      bindPet.setUUID("petId", pet.id)
+      bindPet.setString("name", pet.name)
+      bindPet.setLong("categoryId", pet.categoryId)
+      bindPet
+    }
+  }
+
+  private def insertPet(pet: Pet) = {
+    for {
+      insertPetStatement <- doInsertPet(pet)
+    } yield List(insertPetStatement)
+  }
+}
--- pet-impl/src/main/scala/com/sterowney/pet/impl/PetEvents.scala
+++ pet-impl/src/main/scala/com/sterowney/pet/impl/PetEvents.scala
@@ -1,14 +1,15 @@
 package com.sterowney.pet.impl
 
-import com.lightbend.lagom.scaladsl.persistence.{AggregateEvent, AggregateEventTag}
+import com.lightbend.lagom.scaladsl.persistence.{AggregateEvent, AggregateEventShards, AggregateEventTag}
 import play.api.libs.json.{Format, Json}
 
 sealed trait PetEvent extends AggregateEvent[PetEvent] {
-  def aggregateTag: AggregateEventTag[PetEvent] = PetEvent.Tag
+  def aggregateTag: AggregateEventShards[PetEvent] = PetEvent.Tag
 }
 
 object PetEvent {
-  val Tag = AggregateEventTag[PetEvent]
+  val NumShards = 20 // TODO - configure me
+  val Tag = AggregateEventTag.sharded[PetEvent](NumShards)
 }
 
 case class PetCreated(pet: Pet) extends PetEvent
